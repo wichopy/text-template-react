0dbaf3a20b69b7fb67c3767582a553ceb920ac0d
William Chou <William Chou>
2017-04-16 11:01:55 -0400

    styled with bootstrap, tested with larger clause

--- src/InputFields.jsx
+++ src/InputFields.jsx
@@ -6,7 +6,7 @@ const InputFields = (props) => {
       {props.inputFields.map((inputField, i) => {
         return ( 
           <div key={ i } >
-            <p><b> Field {i} </b> </p> 
+            <p className="field-label"><b> Field {i} </b> </p> 
             <p className="note"> Original value:{ inputField } </p>
             <input onChange={(e)=>props.handleChange(i,inputField,e)}/> 
           </div>
